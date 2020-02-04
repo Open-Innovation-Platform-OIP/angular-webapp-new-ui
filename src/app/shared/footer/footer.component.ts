2433a559a95905fa1d26a8c43b70d2ad3eb60c3a
Vipin Saini <vipin@jaaga.in>
2020-02-04 18:11:34 +0530

    style created scss for footer added min-height to main container

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-cmp',
-  templateUrl: 'footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer-cmp',
+    templateUrl: 'footer.component.html',
+    styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  year: Date = new Date();
+    year: Date = new Date();
 }
