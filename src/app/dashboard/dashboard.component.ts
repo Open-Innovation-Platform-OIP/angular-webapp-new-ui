796e86cc8232be310a8c5f14e3681578b4dfca12
Vipin Saini <vipin@jaaga.in>
2020-02-05 16:30:26 +0530

    style generate scss for dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,7 +16,7 @@ declare const $: any;
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css'],
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
     objectValues = Object['values'];
