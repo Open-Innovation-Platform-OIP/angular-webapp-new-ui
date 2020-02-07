22150771e87fdc0e9d772ca9fd33e6333b773206
Vipin Saini <vipin@jaaga.in>
2020-02-07 14:51:40 +0530

    style created scss for profile card fixed styling for desktop view

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -3,29 +3,38 @@ import { FilesService } from '../../services/files.service';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-profile-card',
-  templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.css']
+    selector: 'app-profile-card',
+    templateUrl: './profile-card.component.html',
+    styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent implements OnInit {
-  @Input() userData: any;
-  @Input() index = 0;
-  personas = [];
+    @Input() userData: any;
+    @Input() index = 0;
+    personas = [];
 
-  constructor(
-    public filesService: FilesService,
-    public authService: AuthService
-  ) {}
+    constructor(
+        public filesService: FilesService,
+        public authService: AuthService
+    ) {}
 
-  ngOnInit() {
-    this.fillPersona();
-  }
+    ngOnInit() {
+        this.fillPersona();
+    }
 
-  fillPersona() {
-    Object.entries(this.userData).map(data => {
-      if (data[0].startsWith('is_') && data[1]) {
-        this.personas.push(data[0].slice(3));
-      }
-    });
-  }
+    fillPersona() {
+        Object.entries(this.userData).map(data => {
+            if (data[0].startsWith('is_') && data[1]) {
+                const persona = data[0].slice(3);
+                this.personas.push(this.capitalize(persona));
+            }
+        });
+    }
+
+    capitalize(word: string): string {
+        if (word.toLowerCase() === 'ngo') {
+            return word.toUpperCase();
+        } else {
+            return word[0].toUpperCase() + word.slice(1);
+        }
+    }
 }
