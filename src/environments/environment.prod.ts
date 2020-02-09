860fc266a6e6e6ef906893964e8fba86dc6e737e
Tej Pochiraju <tej@jaaga.in>
2020-02-09 17:20:13 +0530

    add env variables to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 const environment = {
-    production: false,
+    production: true,
 };
 
 const domain = 'https://oip-dev.dev.jaagalabs.com';
+
+const inviteEndpoint = 'https://invite-flow-microservice-test.dev.jaagalabs.com/invite_user';
+
+const smsEndpoint = 'https://sa-sms-microservice.dev.jaagalabs.com/send';
+
+const authEndpoint = 'https://sa-auth-dev.dev.jaagalabs.com/auth/';
+
+const hasuraEndpoint = 'https://sa-dev.dev.jaagalabs.com/v1/graphql';
+
+const searchEndpoint = 'https://elasticsearch-microservice.dev.jaagalabs.com/global_search';
 
 const fileUploadVariables = {
     UploadUrlEndpoint:
@@ -12,4 +26,13 @@ const fileUploadVariables = {
     deleteEndpoint: 'https://minio-microservice.dev.jaagalabs.com/delete_file',
 };
 
-export { environment, domain, fileUploadVariables };
+export { environment, domain, inviteEndpoint, smsEndpoint, authEndpoint, hasuraEndpoint, searchEndpoint, fileUploadVariables };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
