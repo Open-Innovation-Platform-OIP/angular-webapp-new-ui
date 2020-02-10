e2586f92154bff139493b8dbcdfc9aa0b044d984
Vipin Saini <vipin@jaaga.in>
2020-02-10 19:18:41 +0530

    style fixed styling of enrichment view both tabs

--- src/app/components/enrichment-detail-modal/enrichment-detail-modal.component.ts
+++ src/app/components/enrichment-detail-modal/enrichment-detail-modal.component.ts
@@ -2,30 +2,25 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FilesService } from '../../services/files.service';
 
 @Component({
-  selector: 'app-enrichment-detail-modal',
-  templateUrl: './enrichment-detail-modal.component.html',
-  styleUrls: ['./enrichment-detail-modal.component.scss']
+    selector: 'app-enrichment-detail-modal',
+    templateUrl: './enrichment-detail-modal.component.html',
+    styleUrls: ['./enrichment-detail-modal.component.scss'],
 })
 export class EnrichmentDetailModalComponent implements OnInit {
+    @Input() id = '';
+    @Input() enrichment = {};
+    keysToShow = {
+        extent: 'Extent',
+        impact: 'Impact',
+        organization: 'Organization',
+        beneficiary_attributes: 'Beneficiary Attributes',
+        resources_needed: 'Resources Needed',
+    };
+    objectKeys = Object.keys;
+    indexInFocus = 0;
+    constructor(public filesService: FilesService) {}
 
-  @Input() id = "";
-  @Input() enrichment = {};
-  keysToShow = {
-    description: 'Description',
-    extent: 'Extent',
-    impact: 'Impact',
-    organization: 'Organization',
-    beneficiary_attributes: 'Beneficiary Attributes',
-    resources_needed: 'Resources Needed'
-  };
-  objectKeys = Object.keys;
-  indexInFocus = 0;
-  constructor(
-    public filesService: FilesService,
-  ) { }
-
-  ngOnInit() {
-    // console.log(this.enrichment['attachments']);
-  }
-
+    ngOnInit() {
+        console.log(this.enrichment);
+    }
 }
